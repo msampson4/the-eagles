95b35a0734d3af530d6dd831e076fa82203e177d
arsos <AHadjigeorgiou3@uclan.ac.uk>
2023-03-20 20:08:32 +0200

    Testing.html and Testing.js

--- Testing.js
+++ Testing.js
@@ -2,7 +2,7 @@ const mylist =['Hello' , 'Avatar' , 'Basketball'];
 testlist(mylist)
 function testlist(list)
     {
-    if(list_length == 0)
+    if(list_length === 0)
     {
         console.log("The list is Empty.");
     }
@@ -11,32 +11,66 @@ function testlist(list)
         console.log("The list has more than 1 available games.");
     }
 }
-
-function testGameStart(game)
-    {
-    if (game.state !== "Start")
-    {
+const mystart =[]
+TestStart(mystart)
+function TestStart(start) {
+    if (start.state !== "Start") {
         console.log("The specified treasure hunt is not active right now.")
     }
-    if (game.question !== "empty")
-    {
+    if (start.question !== "empty") {
         console.log("The specified treasure hunt is empty")
     }
-    if (game.player !== 0)
-    {
+    if (start.player !== 0) {
         console.log("The specified playerName:Homer, is already in use")
     }
 }
-function testAnswers(useranswers , corectanswers){
+const myvalue =[]
+TestValue(myvalue)
+function TestValue(value) {
+    return value === undefined,  value === null,  value === '';
+}
+const myid =[]
+TestId(myid)
+function TestId(id)
+{
+    var id = document.getElementById('red');
+    if(id)
+    {
+        alert("id exists!");
+    }
+    else
+    {
+        alert("id doesn't exist");
+    }
+const myparameter =[]
+TestParameter(myparameter)
+    function TestParameter(parameter)
+    {
+        parameter = parameter != null ? parameter : "default";
+        console.log('Multiple error messages of the form', 'Missing or empty parameter');
+    }
+    return parameter;
+    }
+const myquestion =[]
+TestQuestion(myquestion)
+function TestQuestion(){
+
+}
+
+
+const myanswers =[]
+TestAnswers(myanswers)
+function TestAnswers(corectAnswers , userAnswers) {
     let numCorrect = 0;
 
-    userAnswers.forEach((answer, i)=> {
-        if (answer == correctAnswers[i]){
+    userAnswers.forEach((answer, i) => {
+        if (answer == correctAnswers[i]) {
             numCorrect++;
         }
-});
-return numCorrect;
+    });
+    return numCorrect;
 }
+
 
 
 
