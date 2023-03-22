7088c08269dbd12f1e3446b6888daf425531481e
arsos <AHadjigeorgiou3@uclan.ac.uk>
2023-03-22 12:53:46 +0200

    Scoreboard changes

--- Scoreboard/Scoreboard.js
+++ Scoreboard/Scoreboard.js
@@ -1,8 +1,42 @@
-const URL = "https://codecyprus.org/th/api/list";
-fetch(URL)
-    .then(response => response.blob())
-    .then(data => {
-        console.log(data);
-        // Do something with the data
-    })
-    .catch(error => console.error(error));
+const nameTextElement = document.getElementById("nameText");
+const scoreFieldElement = document.getElementById("scoreField");
+getname();
+function getname() {
+
+    const URL ="https://codecyprus.org/th/api/name?session=" + localStorage.getItem("session-id");
+    fetch(URL)
+        .then(response => response.json())
+        .then(jsonObject => {
+            if (jsonObject.status === "OK") {
+                const nameText = jsonObject.nameText;
+                nameTextElement.innerHTML = nameText;
+            } else {
+                alert(jsonObject.errorMessages[0]);
+            }
+        });
+
+
+}
+getscore();
+function getscore(){
+
+    const URL ="https://codecyprus.org/th/api/score?session=" + localStorage.getItem("session-id");
+    fetch(URL)
+        .then(response => response.json())
+        .then(jsonObject => {
+            if (jsonObject.status === "OK") {
+                const scoreField = jsonObject.scoreField;
+                scoreFieldElement.innerHTML = scoreField;
+            } else {
+                alert(jsonObject.errorMessages[0]);
+            }
+        });
+
+
+}
+
+
+
+
+
+
